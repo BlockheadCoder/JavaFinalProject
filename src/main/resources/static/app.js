50851cd0e6649d1452a2099589da9bb3a92f17ce
Thomas Chapman <email.thomaschapman@gmail.com>
2021-04-22 15:34:24 -0400

    trash

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -11,7 +11,7 @@ function connect(){
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame){
         console.log("Connected: "+frame);
-        stompClient.subscribe('/socketOut/'+myId, function (data){
+        stompClient.subscribe('/socketOut/'+myId+'/'+recId, function (data){
             console.log(data);
             var body = JSON.parse(data.body)
             $(".messageHolder").append('<div><h3>'+body.message+'</h3></div>');
@@ -19,6 +19,7 @@ function connect(){
         });
     });
 }
+
 
 function disconnect(){
     if(stompClient !== null){
@@ -29,7 +30,6 @@ function disconnect(){
 
 function sendMessage() {
 
-    userId = $("#userId").val();
     message = $("#message").val();
 
     $(".messageHolder").append('<div class="myMessage"><h3>'+message+'</h3></div>');
