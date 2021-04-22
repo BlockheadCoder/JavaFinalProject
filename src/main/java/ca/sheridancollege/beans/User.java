a917379a17f551440b12343b934f315ce34207cb
krutikov <krutikov@sheridancollege.ca>
2021-04-22 16:56:34 -0400

    1/3 of the way done conversations (doesn't compile)

--- src/main/java/ca/sheridancollege/beans/User.java
+++ src/main/java/ca/sheridancollege/beans/User.java
@@ -2,14 +2,27 @@ package ca.sheridancollege.beans;
 
 import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Builder
 public class User {
-	
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
+	public enum UserType {
+		CUSTOMER,
+		ARTIST
+	}
+	
 	private String name;
+	
+	private UserType type;
 		
 }
--- src/main/java/ca/sheridancollege/controller/MainTroll.java
+++ src/main/java/ca/sheridancollege/controller/MainTroll.java
@@ -1,16 +1,42 @@
 package ca.sheridancollege.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import ca.sheridancollege.beans.User;
+import ca.sheridancollege.repository.UserRepo;
 
 @Controller
 public class MainTroll {
 	
+	@Autowired
+	private UserRepo userRepo;
+	
+	private List<User> allUsers = new ArrayList<User>();
+	private List<User> currentUsers = new ArrayList<User>();
 	
 	@GetMapping("/")
 	public String homeTroll() {
+		
+		currentUsers.addAll(Arrays.asList(new User[] {
+				User.builder().name("Thomas").type(User.UserType.ARTIST).build(),
+				User.builder().name("Roman").type(User.UserType.ARTIST).build()
+			}));
+		
+		allUsers.addAll(Arrays.asList(new User[] {
+				User.builder().name("Fern").type(User.UserType.ARTIST).build(),
+				User.builder().name("Andy").type(User.UserType.CUSTOMER).build()
+		}));
+		
+		allUsers.addAll(currentUsers);
 		
 		return "login.html";
 	}
@@ -21,11 +47,42 @@ public class MainTroll {
 			@RequestParam("recipient") String recipient,
 			Model model) {
 		
+		for (User user : allUsers) {
+			
+		}
 		
 		model.addAttribute("myId",name);
 		model.addAttribute("recId",recipient);
 		
 		return "index.html";
 	}
+	
+	@GetMapping("/conversation")
+	public String conversation(Model model) {
+		
+		model.addAttribute("currentConversations", currentUsers);
+		
+		return "conversation.html";
+	}
+	
+	@GetMapping("/conversationAdd")
+	public String conversationAdd(Model model) {
+		
+		model.addAttribute("currentConversations", allUsers);
+		
+		return "conversationAdd.html";
+	}
+	
+	@PostMapping("/conversationAdd")
+	public String conversationAddForm(Model model) {
+		
+		
+		
+		model.addAttribute("currentConversations", allUsers);
+		
+		return "conversationAdd.html";
+	}
+	
+
 		
 }
