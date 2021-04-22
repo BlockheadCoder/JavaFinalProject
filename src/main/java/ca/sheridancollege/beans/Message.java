ccfabf28f7869171c829bbc47dc0f9a565127a07
fernando <fedelef@sheridancollege.ca>
2021-04-22 19:58:09 -0400

    added postgresql connectivity

--- src/main/java/ca/sheridancollege/beans/Message.java
+++ src/main/java/ca/sheridancollege/beans/Message.java
@@ -1,5 +1,7 @@
 package ca.sheridancollege.beans;
 
+import java.util.Date;
+
 import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,8 +11,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Entity
+@Table(name="\"messages\"")
 public class Message {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
 	
+	private String message;
+	
+	@Column(name="\"senderId\"")
+	private String senderId;
+	
+	@Column(name="\"receiverId\"")
+	private String receiverId;
+	
+	private Boolean seen;
+	private Date created_at; //changed to date
 	
 	public Message(String input, String senderId, String receiverId) {
 		this.message = input;
@@ -21,16 +38,4 @@ public class Message {
 	public Message(String string) {
 		this.message = string;
 	}
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	
-	private String message;
-	private String senderId;
-	private String receiverId;
-	private Boolean seen;
-	private String created_at;
-	
-	
 }
--- src/main/java/ca/sheridancollege/beans/User.java
+++ src/main/java/ca/sheridancollege/beans/User.java
@@ -3,13 +3,14 @@ package ca.sheridancollege.beans;
 import javax.persistence.*;
 
 @Entity
+@Table(name="\"user\"")
 public class User {
-	
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private Integer id;
 	
+	@Column(name="email")
 	private String name;
-		
 }
