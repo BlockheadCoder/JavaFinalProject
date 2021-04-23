97eb0b319282cf53016523fc074bf0f63bfb5972
fernando <fedelef@sheridancollege.ca>
2021-04-22 23:11:58 -0400

    andy's stylish update

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -9,8 +9,10 @@ function connect(){
         stompClient.subscribe('/socketOut/'+myId+'/'+recId, function (data){
             console.log(data);
             var body = JSON.parse(data.body)
-            $(".messageHolder").append('<div><h3>'+body.message+'</h3></div>');
+            $(".messageHolder").append('<div class="messageLeft"><div class="theirMessage"><h3>'+body.message+'</h3></div></div>');
 
+			var d = $(".messageHolder");
+    		d.scrollTop(d.prop("scrollHeight"));
         });
     });
 }
@@ -27,7 +29,10 @@ function sendMessage() {
 
     message = $("#message").val();
 
-    $(".messageHolder").append('<div class="myMessage"><h3>'+message+'</h3></div>');
+    $(".messageHolder").append('<div class="messageRight"><div class="myMessage"><h3>'+message+'</h3></div></div>');
+
+	var d = $(".messageHolder");
+    d.scrollTop(d.prop("scrollHeight"));
 
     console.log("hello");
 
